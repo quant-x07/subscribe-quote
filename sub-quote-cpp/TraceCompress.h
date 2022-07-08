fe2f8447002daa4d9bf3296ad2f04a96c01cb233
netsec <zy.netsec@gmail.com>
2022-07-08 09:30:33 +0800

    * remove compress logic

--- sub-quote-cpp/TraceCompress.h
+++ sub-quote-cpp/TraceCompress.h
@@ -4,10 +4,8 @@
 class TraceCompress {
 public:
     TraceCompress();
-    void compress(const struct Trace &k, uint8_t *buf, int *len);
     void decompress(struct Trace &k, const uint8_t *buf, int *len);
 
-    int pushDynInt(uint8_t *buf, int delta, float orig, uint8_t *f);
     int popDynInt(const uint8_t *buf, float *val, uint8_t flag, uint32_t base, uint16_t prec);
 };
 
